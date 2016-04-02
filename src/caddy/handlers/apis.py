f8c5721580bbba9ec47e1edca2558698d02a7b74
Krishna Teja <kt@hungerbox.com>
2016-04-02 07:03:41 +0530

    pnr

--- src/caddy/handlers/apis.py
+++ src/caddy/handlers/apis.py
@@ -17,7 +17,28 @@ class PNRHandler(RequestHandler):
         status = 200
 
         try:
-            
+            pnr = self.request.get_argument('pnr')
+            assert pnr, {
+            "message" : "PNR not provided",
+            "status" : 400
+            }
+
+            db = self.application.db
+            item = db.pnr.find_one({'pnr': pnr})
+            assert item, {
+            "message" : "Invalid PNR",
+            "status" : 400
+            }
+
+            cities = json.load(open('cities.json'))
+            if item.get('to') not in cities:
+                assert False, {
+                "message": "Uber not available at this city",
+                "status": 400
+                }
+
+            item.pop('_id')
+            response = item
 
         except AssertionError, e:
             e = e.message
